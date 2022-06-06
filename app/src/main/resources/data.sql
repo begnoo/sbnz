332c7c1a863dc52f37f8209a5a3d7187833aacee
marko16 <marko.suljak80@gail.com>
2022-06-07 00:12:20 +0200

    added logic for fault codes P0171 and P0420

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -34,8 +34,8 @@ INSERT INTO answer (id, answer_text) VALUES (7, 'Smells like');
 
 INSERT INTO question_answers (question_id, answers_id) VALUES (3, 5), (3, 6), (3, 7);
 
--- answer count = 13
--- auestion count = 8
+-- answer count = 23
+-- auestion count = 11
 
 -- 
 -- Insturctions
@@ -51,8 +51,25 @@ INSERT INTO instructions (id, part, instructions) VALUES (4, 'MAJOR_DRIVE_BELT',
 INSERT INTO question (id, question) VALUES (5, 'What is your OBD fault code?');
 INSERT INTO question (id, question) VALUES (6, 'Does your car ride roughly?');
 INSERT INTO question (id, question) VALUES (8, 'Does it leak under your vechicle?');
+INSERT INTO question (id, question) VALUES (10, 'Do you rotten eggs/sulphur?');
+INSERT INTO question (id, question) VALUES (12, 'Does your PCV valve leak?');
+
 INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (9, 'P0300', 6);
 INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (18, 'P0440', 8);
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (19, 'P0420', 10);
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (24, 'P0171', 12);
+
+INSERT INTO question (id, question) VALUES (11, 'Does the vechicle start every time?');
+INSERT INTO question (id, question) VALUES (13, 'Is the engine working properly?');
+
+INSERT INTO answer (id, answer_text) VALUES (20, 'Yes');
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (21, 'No', 11);
+INSERT INTO answer (id, answer_text) VALUES (22, 'Yes');
+INSERT INTO answer (id, answer_text) VALUES (23, 'No');
+INSERT INTO answer (id, answer_text) VALUES (25, 'Yes');
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (26, 'No', 13);
+INSERT INTO answer (id, answer_text) VALUES (27, 'Yes');
+INSERT INTO answer (id, answer_text) VALUES (28, 'No');
 
 INSERT INTO question (id, question) VALUES (7, 'Do your spark plugs work?');
 INSERT INTO question (id, question) VALUES (9, 'Is your vapor sensor faulty?');
@@ -62,12 +79,14 @@ INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (15, 'No', 9)
 INSERT INTO answer (id, answer_text) VALUES (11, 'Yes'), (12, 'No'), (13, 'No'), (14, 'Yes'), (16, 'Yes'), (17, 'No');
 
 INSERT INTO question_answers (question_id, answers_id) VALUES (5, 9), (5, 18), (6, 10), (6, 13), (7, 11), (7, 12), (8, 14), (8, 15), (9, 16), (9, 17);
+INSERT INTO question_answers (question_id, answers_id) VALUES (5, 19), (10, 20), (10, 21), (11, 22), (11, 23);
+INSERT INTO question_answers (question_id, answers_id) VALUES (5, 24), (12, 25), (12, 26), (13, 27), (13, 28);
 
 INSERT INTO instructions (id, part, instructions) VALUES (5, 'SPARK_PLUGS', 'Change your spark plugs.');
 INSERT INTO instructions (id, part, instructions) VALUES (6, 'RESET_OBD', 'OBD connector made an error. Restart it.');
 INSERT INTO instructions (id, part, instructions) VALUES (7, 'FUEL_TANK', 'Your fuel tank is probably leaking. Visit your mechanic.');
 INSERT INTO instructions (id, part, instructions) VALUES (8, 'VAPOR_SENSOR', 'Change your vapor sensor.');
-
-
-
-
+INSERT INTO instructions (id, part, instructions) VALUES (9, 'CATALYZER', 'Check if your catalyzer is working properly.');
+INSERT INTO instructions (id, part, instructions) VALUES (10, 'FUEL_PUMP', 'Check if your fuel pump is working properly.');
+INSERT INTO instructions (id, part, instructions) VALUES (11, 'MAF', 'Your Mass Air Flow sensor is probably faulty.');
+INSERT INTO instructions (id, part, instructions) VALUES (12, 'PCV_LEAK', 'Your PCV valve is faulty.');
