8192add59857f2e3de44af2a0104d78f94ca2ce0
marko16 <marko.suljak80@gail.com>
2022-06-06 19:55:22 +0200

    added logic for obd fault code p0440

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -34,8 +34,8 @@ INSERT INTO answer (id, answer_text) VALUES (7, 'Smells like');
 
 INSERT INTO question_answers (question_id, answers_id) VALUES (3, 5), (3, 6), (3, 7);
 
--- answer count = 8
--- auestion count = 10
+-- answer count = 13
+-- auestion count = 8
 
 -- 
 -- Insturctions
@@ -50,17 +50,24 @@ INSERT INTO instructions (id, part, instructions) VALUES (4, 'MAJOR_DRIVE_BELT',
 -- 
 INSERT INTO question (id, question) VALUES (5, 'What is your OBD fault code?');
 INSERT INTO question (id, question) VALUES (6, 'Does your car ride roughly?');
+INSERT INTO question (id, question) VALUES (8, 'Does it leak under your vechicle?');
 INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (9, 'P0300', 6);
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (18, 'P0440', 8);
 
 INSERT INTO question (id, question) VALUES (7, 'Do your spark plugs work?');
+INSERT INTO question (id, question) VALUES (9, 'Is your vapor sensor faulty?');
 INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (10, 'Yes', 7);
 
-INSERT INTO answer (id, answer_text) VALUES (11, 'Yes'), (12, 'No'), (13, 'No');
+INSERT INTO answer (id, answer_text, follow_up_question_id) VALUES (15, 'No', 9);
+INSERT INTO answer (id, answer_text) VALUES (11, 'Yes'), (12, 'No'), (13, 'No'), (14, 'Yes'), (16, 'Yes'), (17, 'No');
 
-INSERT INTO question_answers (question_id, answers_id) VALUES (5, 9), (6, 10), (6, 13), (7, 11), (7, 12);
+INSERT INTO question_answers (question_id, answers_id) VALUES (5, 9), (5, 18), (6, 10), (6, 13), (7, 11), (7, 12), (8, 14), (8, 15), (9, 16), (9, 17);
 
 INSERT INTO instructions (id, part, instructions) VALUES (5, 'SPARK_PLUGS', 'Change your spark plugs.');
 INSERT INTO instructions (id, part, instructions) VALUES (6, 'RESET_OBD', 'OBD connector made an error. Restart it.');
+INSERT INTO instructions (id, part, instructions) VALUES (7, 'FUEL_TANK', 'Your fuel tank is probably leaking. Visit your mechanic.');
+INSERT INTO instructions (id, part, instructions) VALUES (8, 'VAPOR_SENSOR', 'Change your vapor sensor.');
+
 
 
 
