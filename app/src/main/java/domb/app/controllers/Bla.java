a5f6364f45e32ea4749e345bebac5e7151d7cb6a
begnoo <zoranjankov99@gmail.com>
2022-09-08 00:08:01 +0200

    fix: linux chmod build-template.bat

--- app/src/main/java/domb/app/controllers/Bla.java
+++
@@ -1,7 +0,0 @@
-package domb.app.controllers;
-
-public class Bla {
-    public static void printEvent(String nekiEvent) {
-        System.out.println("ovo je iz bla: " + nekiEvent);
-    }
-}
--- app/src/main/java/domb/app/controllers/QuestionController.java
+++ app/src/main/java/domb/app/controllers/QuestionController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import domb.app.model.facts.Question;
 import domb.app.services.QuestionService;
 
-import javax.print.attribute.standard.Media;
 
 @RestController
 @RequestMapping("/questions")
